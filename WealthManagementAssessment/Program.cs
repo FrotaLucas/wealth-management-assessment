746abe3cf4945630f698af36cbb032d2cebaba56
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-30 17:32:44 +0200

    first loop

--- WealthManagementAssessment/Program.cs
+++ WealthManagementAssessment/Program.cs
@@ -12,6 +12,23 @@ class Program
         string path = "C:\\MyFolders\\Visual_Studio_Projects\\WealthManagementAssessment\\WealthManagementAssessment\\Csv\\Investments.csv";
 
         string investmentId = "Investor30";
+
+        using (var reader = new StreamReader(path))
+        {
+            string? line;
+            int count = 0;
+
+            while( (line = reader.ReadLine()) != null )
+            {
+                var fields = line.Split(';');
+                var investor = fields[0];
+                Console.WriteLine($"id: {investor}");
+
+                count++;
+            }
+
+            Console.WriteLine($"total: {count}");
+        }
     }
 
 }
