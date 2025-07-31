4796a726caef68db06c03104f0ad1a0105221612
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-31 11:37:18 +0200

    add class Investment

---
+++ WealthManagementAssessment/Domain/Investment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WealthManagementAssessment.Domain
+{
+    internal class Investment
+    {
+    }
+}
