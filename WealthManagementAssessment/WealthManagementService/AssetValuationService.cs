669b90c3939ff2e7f92be5d9900dd4e5edb5f470
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-04 17:45:35 +0200

    new property in class

--- WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
+++ WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
@@ -15,10 +15,11 @@ namespace WealthManagementAssessment.WealthManagementService
 
         public double Fonds { get; set; }
 
-      
+        public string OwnerId { get; set; }
+
         public AssetValuationService(string ownerId)
         {
-           
+            OwnerId = ownerId;
         }
 
     }
