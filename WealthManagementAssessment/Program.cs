5a6cda0fc22604080db795b0e97e0778cd6e9b84
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-04 21:13:04 +0200

    adapt date to test

--- WealthManagementAssessment/Program.cs
+++ WealthManagementAssessment/Program.cs
@@ -6,16 +6,16 @@ class Program
     private static void Main(string[] args)
     {
         string investorId = "Investor90";
-        string dateString = "08-04-2025";
+        string dateString = "04-26-2020";
 
         //var line = Console.ReadLine();
         //var steps = line.Split(";");
         DateTime date = DateTime.Parse(dateString);
 
-        Console.WriteLine("date format" + date);
+        //Console.WriteLine("date format" + date);
         //8/4/2025 12:00:00 AM
 
-        var obj = new AssetValuationService(investorId);
+        var obj = new AssetValuationService(investorId, date);
 
         Console.WriteLine("total investments: " + obj.TotalInvestments());
         Console.WriteLine("total Bulding Investor90: " + obj.RealStateEngine());
