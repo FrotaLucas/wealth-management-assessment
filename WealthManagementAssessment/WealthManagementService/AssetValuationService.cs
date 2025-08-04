bffa5b0227a0f609e4e26651b197fbab4dc286ec
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-04 17:40:42 +0200

    set class public

--- WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
+++ WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace WealthManagementAssessment.WealthManagementService
 {
-    internal class AssetValuationService
+    public class AssetValuationService
     {
+
     }
 }
