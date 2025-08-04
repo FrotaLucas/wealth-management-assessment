88e584bc32d0bc1ae174b9e478d8503d5e3d819e
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-04 17:40:23 +0200

    fix name of class

--- WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
+++ WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WealthManagementAssessment.WealthManagementService
 {
-    internal class AsserValuationService
+    internal class AssetValuationService
     {
     }
 }
