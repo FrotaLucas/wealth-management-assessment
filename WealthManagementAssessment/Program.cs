d6854d4c3ed0feb95a2a9041b744e7db749a72a9
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-04 19:31:23 +0200

    Datetime

--- WealthManagementAssessment/Program.cs
+++ WealthManagementAssessment/Program.cs
@@ -6,6 +6,14 @@ class Program
     private static void Main(string[] args)
     {
         string investorId = "Investor90";
+
+        //var line = Console.ReadLine();
+        //var steps = line.Split(";");
+        //DateTime date = DateTime.Parse(steps[1]);
+
+        //Console.WriteLine("date format"+ date);
+        //12/28/2015 12:00:00 AM
+
         var obj = new AssetValuationService(investorId);
 
         Console.WriteLine("total investments: " + obj.TotalInvestments());
