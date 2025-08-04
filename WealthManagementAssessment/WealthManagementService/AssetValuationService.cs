4202814c2167af7829c513e32d84faf5b79c614a
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-04 18:04:51 +0200

    set field static

--- WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
+++ WealthManagementAssessment/WealthManagementService/AssetValuationService.cs
@@ -9,6 +9,12 @@ namespace WealthManagementAssessment.WealthManagementService
     public class AssetValuationService
     {
 
+
+        static string baseDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+        string fileInvestments = Path.Combine(baseDirectory, "Csv\\Investments.csv");
+        string fileTransactions = Path.Combine(baseDirectory, "Csv\\Transactions.csv");
+
+
         public string OwnerId { get; set; }
 
         public AssetValuationService(string ownerId)
