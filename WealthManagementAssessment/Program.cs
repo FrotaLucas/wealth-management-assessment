98ea439bd855e080bd4cb941b275d83abb0c5fe7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-05 13:43:24 +0200

    new date item

--- WealthManagementAssessment/Program.cs
+++ WealthManagementAssessment/Program.cs
@@ -6,14 +6,14 @@ class Program
     private static void Main(string[] args)
     {
         string investorId = "Investor90";
-        string dateString = "04-26-2020";
+        string dateString = "01-16-2016";
 
         //var line = Console.ReadLine();
         //var steps = line.Split(";");
         DateTime date = DateTime.Parse(dateString);
 
         Console.WriteLine("DateTime format: " + date);
-        //4/26/2020 12:00:00 AM
+        //1/16/2016 12:00:00 AM
 
         var obj = new AssetValuationService(investorId, date);
 
