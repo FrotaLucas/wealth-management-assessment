88bcd1e66b7b22799d52489aea475cecde66b67b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-07 15:36:30 +0200

    baseDirectoryString

--- WealthManagementAssessment/WealthManagementService/AssetValuation.cs
+++ WealthManagementAssessment/WealthManagementService/AssetValuation.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WealthManagementAssessment.Domain;
 
 namespace WealthManagementAssessment.WealthManagementService
 {
     public class AssetValuation
     {
 
+        static string baseDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+        string fileInvestments = Path.Combine(baseDirectory, "Csv\\Investments.csv");
+        string fileTransactions = Path.Combine(baseDirectory, "Csv\\Transactions.csv");
+        public Investor Investor { get; set; }
         public AssetValuation()
         {
             
         }
         public void FileReader()
         {
-
+           
         }
 
     }
