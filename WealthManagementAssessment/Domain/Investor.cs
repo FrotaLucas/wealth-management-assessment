227d71752706a7fe3fa152609b180878a0426d57
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-07 12:17:47 +0200

    clean class Investo

--- WealthManagementAssessment/Domain/Investor.cs
+++ WealthManagementAssessment/Domain/Investor.cs
@@ -4,14 +4,6 @@
     {
         public string InvestorId { get; set; }
 
-        public string InvestmentType { get; set; }
-
-        public string Isin { get; set; }
-
-        public string City { get; set; }
-
-        public string FondsInvestor { get; set; }
-
         public List<Investment> Investments { get; set; }
     }
 }
