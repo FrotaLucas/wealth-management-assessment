f3f17e507c7916c543c1cc228677c10a714d8675
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-07 12:13:44 +0200

    clean class investor

--- WealthManagementAssessment/Domain/Investor.cs
+++ WealthManagementAssessment/Domain/Investor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WealthManagementAssessment.Domain
+﻿namespace WealthManagementAssessment.Domain
 {
     public class Investor
     {
