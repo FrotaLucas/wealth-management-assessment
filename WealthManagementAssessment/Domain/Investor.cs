f096d4327def18911057fe93c8bb32eeb882557b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-07 12:00:21 +0200

    new class Investor

---
+++ WealthManagementAssessment/Domain/Investor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WealthManagementAssessment.Domain
+{
+    public class Investor
+    {
+        public string InvestorId { get; set; }
+
+        public List<Investment> Investments { get; set; }
+    }
+}
