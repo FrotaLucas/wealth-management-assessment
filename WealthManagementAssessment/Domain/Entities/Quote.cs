8209881356a149c7d7d2c60f5864c9bf3164589b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-27 11:22:18 +0200

    move entity

--- WealthManagementAssessment/Domain/Entities/Quote.cs
+++ WealthManagementAssessment/Domain/Entities/Quote.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WealthManagementAssessment.Domain
+namespace WealthManagementAssessment.Domain.Entities
 {
     public class Quote
     {
