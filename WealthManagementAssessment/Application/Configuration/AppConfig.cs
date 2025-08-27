47befb7604c7a6c704cb5d21a4a54cb4bc75ad09
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-27 21:42:41 +0200

    implement methods of class

---
+++ WealthManagementAssessment/Application/Configuration/AppConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WealthManagementAssessment.Application.Configuration
+{
+    public class AppConfig
+    {
+
+        sealed class DataFileCsv
+        {
+            public string InvestmentsFile { get; set; }
+
+            public string TransactionsFile { get; set; }
+
+            public string QuotesFile { get; set; }
+
+        }
+    }
+}
