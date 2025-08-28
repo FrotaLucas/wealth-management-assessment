a9454211085a127209fe3fea21343d28eac3dc55
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-28 23:37:36 +0200

    clean unused packages

--- WealthManagementAssessment/Domain/Contracts/Interfaces/IFilesReader.cs
+++ WealthManagementAssessment/Domain/Contracts/Interfaces/IFilesReader.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WealthManagementAssessment.Domain.Entities;
+﻿using WealthManagementAssessment.Domain.Entities;
 
 namespace WealthManagementAssessment.Domain.Contracts.Interfaces
 {
