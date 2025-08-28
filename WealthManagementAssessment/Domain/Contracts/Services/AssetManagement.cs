55a36beaa8a16d71991554f1a90452b54727d748
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-28 17:43:07 +0200

    implement class

--- WealthManagementAssessment/Domain/Contracts/Services/AssetManagement.cs
+++ WealthManagementAssessment/Domain/Contracts/Services/AssetManagement.cs
@@ -1,6 +1,23 @@
-﻿namespace WealthManagementAssessment.Domain.Contracts.Services
+﻿using WealthManagementAssessment.Domain.Contracts.Interfaces;
+
+namespace WealthManagementAssessment.Domain.Contracts.Services
 {
     public class AssetManagement
     {
+
+        private readonly IAssetValuation _assetValuation;
+
+        public AssetManagement (IAssetValuation assetValuation)
+        {
+            _assetValuation = assetValuation;
+        }
+
+
+
+        public void DisplayAsset()
+        {
+
+        }
     }
+
 }
