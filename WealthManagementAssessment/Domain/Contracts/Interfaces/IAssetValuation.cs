02ed589ef1aa5ff3b2a54902c13676c9028885e2
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-28 17:20:16 +0200

    create interface

---
+++ WealthManagementAssessment/Domain/Contracts/Interfaces/IAssetValuation.cs
@@ -0,0 +1,25 @@
+﻿using WealthManagementAssessment.Domain.Entities;
+
+namespace WealthManagementAssessment.Domain.Contracts.Interfaces
+{
+    public interface IAssetValuation
+    {
+
+        List<Investment> ReadInvestments(string ownerId);
+
+        void ReadTransactions(List<Investment> investments);
+
+        List<Quote> ReadQuotes(List<Investment> investments);
+
+        void FilesReader();
+
+
+        double RealStateEngine(List<Investment> investments);
+
+        double StockEngine(List<Investment> investments);
+
+        double FondEngine();
+
+        void AssetEngine();
+    }
+}
