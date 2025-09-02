c7cec1e322628e92f8d6ee108327959d959766b5
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-02 19:09:50 +0200

    refactor read investmens

--- WealthManagementAssessment/Infrastructure/Repository/FilesReader.cs
+++ WealthManagementAssessment/Infrastructure/Repository/FilesReader.cs
@@ -20,24 +20,45 @@ namespace WealthManagementAssessment.Infrastructure.Repository
 
         public List<Investment> ReadInvestments(string ownerId)
         {
-            var investments = File.ReadLines(_appConfig.CsvPath.Investments)
-                .Skip(1)
-                .Select(line => line.Split(';'))
-                .Where(parts => parts[0] == ownerId)
-                .OrderByDescending(parts => parts[1]) //92.. 82.. 81.. 
-                .Select(parst => new Investment
-                {
-                    InvestmentId = parst[1],
-                    InvestorId = parst[0],
-                    InvestmentType = parst[2],
-                    Isin = parst[3],
-                    City = parst[4],
-                    FondsInvestor = parst[5]
-                }).ToList();
+
+            List<Investment> investments = new List<Investment>();
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                HasHeaderRecord = true,
+                TrimOptions = TrimOptions.Trim, // remove espaços extras nos cabeçalhos e valores
+            };
+
+            using (var reader = new StreamReader(_appConfig.CsvPath.Investments))
+
+            using (var csv = new CsvReader(reader, config))
+            {
+
+                investments = csv.GetRecords<Investment>()
+                     .Where(inv => inv.InvestorId == ownerId)
+                     .ToList();
+
+            }
+            return investments;
+
+            //var investments = File.ReadLines(_appConfig.CsvPath.Investments)
+            //.Skip(1)
+            //.Select(line => line.Split(';'))
+            //.Where(parts => parts[0] == ownerId)
+            //.OrderByDescending(parts => parts[1]) //92.. 82.. 81.. 
+            //.Select(parst => new Investment
+            //{
+            //    InvestmentId = parst[1],
+            //    InvestorId = parst[0],
+            //    InvestmentType = parst[2],
+            //    ISIN = parst[3],
+            //    City = parst[4],
+            //    FondsInvestor = parst[5]
+            //}).ToList();
 
             //Console.WriteLine($"total investment of investor90: {investments.Count}");
 
-            return investments;
+            //return investmentss;
         }
 
 
@@ -57,7 +78,7 @@ namespace WealthManagementAssessment.Infrastructure.Repository
             {
                 //LER TODO ARQUIVO transactions de 700K linhas
                 var allTransactions = csv.GetRecords<Transaction>()
-                    .Where(t => t.Date < valuationDate) 
+                    .Where(t => t.Date < valuationDate)
                     .ToList();
 
                 foreach (var investment in investments)
@@ -92,7 +113,7 @@ namespace WealthManagementAssessment.Infrastructure.Repository
                 foreach (var investment in investments)
                 {
                     var invQuotes = allQuotes
-                        .Where(parts => parts.ISIN == investment.Isin && parts.Date < valuationDate) //cut out unused quote range
+                        .Where(parts => parts.ISIN == investment.ISIN && parts.Date < valuationDate) //cut out unused quote range
                         .OrderBy(quote => quote.ISIN)
                         .ThenByDescending(quote => quote.Date)
                         .ToList();
