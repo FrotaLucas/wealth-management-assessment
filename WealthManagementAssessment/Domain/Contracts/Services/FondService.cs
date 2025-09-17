72966a82bb6e0c6dd1c7f9b1b3f64f1f9ba4d0b8
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 16:30:52 +0200

    create new method

--- WealthManagementAssessment/Domain/Contracts/Services/FondService.cs
+++ WealthManagementAssessment/Domain/Contracts/Services/FondService.cs
@@ -1,4 +1,5 @@
 ﻿using WealthManagementAssessment.Domain.Contracts.Interfaces;
+using WealthManagementAssessment.Domain.Entities;
 
 namespace WealthManagementAssessment.Domain.Contracts.Services
 {
@@ -9,6 +10,52 @@ namespace WealthManagementAssessment.Domain.Contracts.Services
         public FondService(IAssetRepository assetRepository)
         {
             _assetRepository = assetRepository;
+        }
+
+
+
+        public double FondEngine(string ownerId, DateTime valuationDate)
+        {
+            double fondSumup = 0;
+
+            //USAR ENUM ao inves de STRING!!!!!!!!
+            List<Investment> fonds = _assetRepository.GetAllInvestmentsByInvestor(ownerId, valuationDate)
+                .Where(investment => investment.InvestmentType == "Fonds")
+                .ToList();
+
+            //_filesReader.ReadTransactions(fonds, valuationDate);
+
+            //old code
+            //List<Investment> allInvestments = _filesReader.ReadInvestments();
+
+
+            //new code
+            //Dictionary<string, List<Investment>> dictionary = _filesReader.GetDictionary(ownerId, valuationDate);
+            Dictionary<string, List<Investment>> dictionary = GetAllInvestmentsByFonds(ownerId, valuationDate);
+
+
+
+            foreach (var fond in fonds)
+            {
+                double totalPercentage = fond.Transactions.Sum(t => t.Value);
+
+                //old code
+                //List<Investment> investmentsOfFound = allInvestments.Where(i => i.InvestorId == fond.FondsInvestor).ToList();
+                //_filesReader.ReadTransactions(investmentsOfFound, valuationDate);
+
+                //new code
+                dictionary.TryGetValue(fond.FondsInvestor, out var investmentsOfFound);
+
+
+                double realStateSumup = RealStateEngine(investmentsOfFound);
+
+                double stockSumup = StockEngine(investmentsOfFound, valuationDate);
+
+
+                fondSumup = fondSumup + totalPercentage * (realStateSumup + stockSumup);
+            }
+
+            return fondSumup;
         }
     }
 }
