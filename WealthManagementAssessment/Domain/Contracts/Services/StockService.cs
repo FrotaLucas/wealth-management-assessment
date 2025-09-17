14d43ee451cc0e2b58ed1ef09037ab5cc05e01c9
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 15:38:47 +0200

    implement method for class StockService

---
+++ WealthManagementAssessment/Domain/Contracts/Services/StockService.cs
@@ -0,0 +1,51 @@
+﻿using WealthManagementAssessment.Domain.Contracts.Interfaces;
+using WealthManagementAssessment.Domain.Entities;
+
+namespace WealthManagementAssessment.Domain.Contracts.Services
+{
+    public class StockService : IStockService
+    {
+
+        private readonly IAssetRepository _assetRepository;
+
+        public StockService(IAssetRepository assetRepository)
+        {
+            _assetRepository = assetRepository;
+        }
+
+        public double StockEngine(List<Investment> investments, DateTime valuationDate)
+        {
+
+            var stockInvestments = investments
+                .Where(investment => investment.InvestmentType.Equals("Stock"))
+                .ToList();
+
+
+            double stockSumup = 0;
+
+            foreach (var investment in stockInvestments)
+            {
+                double totalShares = investment.Transactions.Sum(transaction => transaction.Value);
+
+                
+                if (!_assetRepository.QuotesByIsin.TryGetValue(investment.ISIN, out var isinQuotes))
+                    continue;
+
+                var quoteToday = isinQuotes.FirstOrDefault(quote => quote.Date <= valuationDate);
+
+                //if valuationDate is too small
+                if (quoteToday == null)
+                    quoteToday = isinQuotes.LastOrDefault();
+
+                var marketValue = totalShares * quoteToday.PricePerShare;
+                stockSumup += marketValue;
+
+            }
+
+            return stockSumup;
+        }
+
+
+    }
+
+}
