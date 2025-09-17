d698744ee7f6cf1259fe309419697b63e4ec2405
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 14:17:16 +0200

    implement realstateservice

---
+++ WealthManagementAssessment/Domain/Contracts/Services/RealStateService.cs
@@ -0,0 +1,19 @@
+﻿using WealthManagementAssessment.Domain.Contracts.Interfaces;
+using WealthManagementAssessment.Domain.Entities;
+
+namespace WealthManagementAssessment.Domain.Contracts.Services
+{
+    public class RealStateService : IRealStateService
+    {
+        public double RealStateEngine(List<Investment> investments)
+        {
+
+            var realStateSumup = investments
+                .Where(investment => investment.InvestmentType == "RealEstate")
+                .SelectMany(investment => investment.Transactions)
+                .Sum(transaction => transaction.Value);
+
+            return realStateSumup;
+        }
+    }
+}
