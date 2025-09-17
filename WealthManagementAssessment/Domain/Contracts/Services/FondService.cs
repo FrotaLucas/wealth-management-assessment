f64f3127b6a1a2fce36064160f372c1574a2b3e1
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 16:24:02 +0200

    clean method

--- WealthManagementAssessment/Domain/Contracts/Services/FondService.cs
+++ WealthManagementAssessment/Domain/Contracts/Services/FondService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WealthManagementAssessment.Domain.Contracts.Interfaces;
+﻿using WealthManagementAssessment.Domain.Contracts.Interfaces;
 
 namespace WealthManagementAssessment.Domain.Contracts.Services
 {
