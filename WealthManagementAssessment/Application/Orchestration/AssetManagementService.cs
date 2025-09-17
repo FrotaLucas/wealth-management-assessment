5cee1ca46e997bb32963239d3defdffc89482458
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 14:11:34 +0200

    implement IportifolioService

--- WealthManagementAssessment/Application/Orchestration/AssetManagementService.cs
+++ WealthManagementAssessment/Application/Orchestration/AssetManagementService.cs
@@ -6,15 +6,11 @@ namespace WealthManagementAssessment.Application.Orchestration
 {
     public class AssetManagementService : IAssetManagement
     {
-        private readonly IPortfolioService _portifolioService;
+        private readonly IPortfolioService _portfolioService;
 
-        private readonly IAssetRepository _assetRepository;
-
-
-        public AssetManagementService(IAssetRepository assetRepository, IPortfolioService portifolioService)
+        public AssetManagementService(IPortfolioService portfolioService)
         {
-            _assetRepository = assetRepository;
-            _portifolioService = portifolioService;
+            _portfolioService = portfolioService;
         }
 
         public void GetFundAsset(string ownerId, DateTime valuationDate)
@@ -26,21 +22,21 @@ namespace WealthManagementAssessment.Application.Orchestration
 
         public void GetRealEstateAsset(string ownerId, DateTime valuationDate)
         {
-            List<Investment> investments = _assetRepository.GetAllInvestmentsByInvestor(ownerId, valuationDate);
+            List<Investment> investments = _portfolioService.GetAllInvestmentsByInvestor(ownerId, valuationDate);
 
-            double asset = _assetRepository.RealStateEngine(investments);
+            //double asset = _assetRepository.RealStateEngine(investments);
 
-            Console.WriteLine($"Your Real Estate wallet is : {asset} Euros.");
+            //Console.WriteLine($"Your Real Estate wallet is : {asset} Euros.");
 
         }
 
         public void GetStockAsset(string ownerId, DateTime valuationDate)
         {
-            List<Investment> investments = _assetRepository.GetAllInvestmentsByInvestor(ownerId, valuationDate);
+            List<Investment> investments = _portfolioService.GetAllInvestmentsByInvestor(ownerId, valuationDate);
 
-            double asset = _assetRepository.StockEngine(investments, valuationDate);
+            //double asset = _assetRepository.StockEngine(investments, valuationDate);
 
-            Console.WriteLine($"Your Stock wallet is : {asset} Euros.");
+            //Console.WriteLine($"Your Stock wallet is : {asset} Euros.");
         }
 
         
@@ -48,11 +44,11 @@ namespace WealthManagementAssessment.Application.Orchestration
         //chamar de balance
         public void GetTotalAsset(string ownerId, DateTime valuationDate)
         {
-            List<Investment> investments = _assetRepository.GetAllInvestmentsByInvestor(ownerId, valuationDate);
+            List<Investment> investments = _portfolioService.GetAllInvestmentsByInvestor(ownerId, valuationDate);
 
-            double realEstateAsset = _assetRepository.RealStateEngine(investments);
+            //double realEstateAsset = _assetRepository.RealStateEngine(investments);
 
-            double stockAsset = _assetRepository.StockEngine(investments, valuationDate);
+            //double stockAsset = _assetRepository.StockEngine(investments, valuationDate);
 
 
             //double fundAsset = _assetRepository.FondEngine(ownerId, valuationDate);
