c1b01a2bd56ee5fcafc9ff17775f07139ab385d7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 14:17:04 +0200

    create interface for realstateservice

---
+++ WealthManagementAssessment/Domain/Contracts/Interfaces/IRealStateService.cs
@@ -0,0 +1,9 @@
+﻿using WealthManagementAssessment.Domain.Entities;
+
+namespace WealthManagementAssessment.Domain.Contracts.Interfaces
+{
+    public interface IRealStateService
+    {
+        double RealStateEngine(List<Investment> investments);
+    }
+}
