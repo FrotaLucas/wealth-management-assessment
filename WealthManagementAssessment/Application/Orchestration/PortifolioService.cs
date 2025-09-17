fc638614b3793d5504a6cf21d91df12d69f61d6e
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 13:38:46 +0200

    implement method

--- WealthManagementAssessment/Application/Orchestration/PortifolioService.cs
+++ WealthManagementAssessment/Application/Orchestration/PortifolioService.cs
@@ -1,9 +1,23 @@
 ﻿using WealthManagementAssessment.Application.Orchestration.Interfaces;
+using WealthManagementAssessment.Domain.Contracts.Interfaces;
+using WealthManagementAssessment.Domain.Entities;
 
 namespace WealthManagementAssessment.Application.Orchestration
 {
     public class PortifolioService : IPortifolioService
     {
+        private readonly IAssetRepository _assetRepository;
 
+        public PortifolioService(IAssetRepository assetRepository)
+        {
+            _assetRepository = assetRepository;
+        }
+
+        public List<Investment> GetAllInvestmentsByInvestor(string ownerId, DateTime valuationDate)
+        {
+            List<Investment> investments = _assetRepository.GetAllInvestmentsByInvestor(ownerId, valuationDate);
+
+            return investments;
+        }
     }
 }
