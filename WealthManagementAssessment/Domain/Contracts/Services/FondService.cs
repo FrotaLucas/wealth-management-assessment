d1ee986dd9714801b97394d2a0efe3e432dd39c6
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 16:23:41 +0200

    implement imterface

---
+++ WealthManagementAssessment/Domain/Contracts/Services/FondService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthManagementAssessment.Domain.Contracts.Interfaces;
+
+namespace WealthManagementAssessment.Domain.Contracts.Services
+{
+    public class FondService : IFondService
+    {
+    }
+}
