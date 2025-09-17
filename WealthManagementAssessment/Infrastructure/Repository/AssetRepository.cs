e9d74fa61cea208c9ba47fb32da865566f41dbf7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-17 13:00:23 +0200

    move up method

--- WealthManagementAssessment/Infrastructure/Repository/AssetRepository.cs
+++ WealthManagementAssessment/Infrastructure/Repository/AssetRepository.cs
@@ -43,6 +43,51 @@ namespace WealthManagementAssessment.Infrastructure.Repository
             return investments;
         }
 
+        public Dictionary<string, List<Investment>> GetAllInvestmentsByFonds(string ownerId, DateTime valuationDate)
+        {
+
+            //TALVEZ NAO PRECISE DESSA LINHA 
+            if (!InvestmentsByOwnerId.TryGetValue(ownerId, out var fonds))
+            {
+                Console.WriteLine("Investments not found for this investor");
+                return new Dictionary<string, List<Investment>>();
+            }
+
+
+            var fondList = new HashSet<string>(fonds
+                .Where(investment => investment.InvestmentType.Equals("Fonds"))
+                .Select(investment => investment.FondsInvestor));
+
+
+            List<Investment> fondInvestment = new List<Investment>();
+            foreach (string fond in fondList)
+            {
+                if (InvestmentsByOwnerId.TryGetValue(fond, out var investments))
+                    fondInvestment.AddRange(investments);
+            }
+
+            var dictionary = fondInvestment
+                .GroupBy(investment => investment.InvestorId)
+                .ToDictionary(group => group.Key, group => group.ToList());
+
+            foreach (var kvp in dictionary)
+            {
+
+                var investments = kvp.Value;
+
+                foreach (var investment in investments)
+                {
+                    if (TransactionsByInvestmentId.TryGetValue(investment.InvestmentId, out var transactions))
+                        investment.Transactions = transactions.Where(transaction => transaction.Date < valuationDate).ToList();
+                    else
+                        investment.Transactions = new List<Transaction>();
+                }
+            }
+
+            return dictionary;
+
+        }
+
         public double RealStateEngine(List<Investment> investments)
         {
 
@@ -132,49 +177,5 @@ namespace WealthManagementAssessment.Infrastructure.Repository
             return fondSumup;
         }
 
-        public Dictionary<string, List<Investment>> GetAllInvestmentsByFonds(string ownerId, DateTime valuationDate)
-        {
-
-            //TALVEZ NAO PRECISE DESSA LINHA 
-            if (!InvestmentsByOwnerId.TryGetValue(ownerId, out var fonds))
-            {
-                Console.WriteLine("Investments not found for this investor");
-                return new Dictionary<string, List<Investment>>();
-            }
-
-
-            var fondList = new HashSet<string>(fonds
-                .Where(investment => investment.InvestmentType.Equals("Fonds"))
-                .Select(investment => investment.FondsInvestor));
-
-
-            List<Investment> fondInvestment = new List<Investment>();
-            foreach (string fond in fondList)
-            {
-                if (InvestmentsByOwnerId.TryGetValue(fond, out var investments))
-                    fondInvestment.AddRange(investments);
-            }
-
-            var dictionary = fondInvestment
-                .GroupBy(investment => investment.InvestorId)
-                .ToDictionary(group => group.Key, group => group.ToList());
-
-            foreach (var kvp in dictionary)
-            {
-
-                var investments = kvp.Value;
-
-                foreach (var investment in investments)
-                {
-                    if (TransactionsByInvestmentId.TryGetValue(investment.InvestmentId, out var transactions))
-                        investment.Transactions = transactions.Where(transaction => transaction.Date < valuationDate).ToList();
-                    else
-                        investment.Transactions = new List<Transaction>();
-                }
-            }
-
-            return dictionary;
-
-        }
     }
 }
