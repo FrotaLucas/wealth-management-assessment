24bdd5017b18df9ba2957d0a40ca796d39c32c26
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-18 21:47:02 +0200

    fix parameter of RealStateEngine

--- WealthManagementAssessment/Domain/Services/FondService.cs
+++ WealthManagementAssessment/Domain/Services/FondService.cs
@@ -35,7 +35,7 @@ namespace WealthManagementAssessment.Domain.Services
                 decimal totalPercentage = fond.Transactions.Sum(t => t.Value);
 
                 dictionary.TryGetValue(fond.FondsInvestor, out var investmentsOfFound);
-                decimal realStateSumup = _realStateService.RealStateEngine(investmentsOfFound);
+                decimal realStateSumup = _realStateService.RealStateEngine(fond.FondsInvestor, valuationDate);
                 decimal stockSumup = _stockService.StockEngine(investmentsOfFound, valuationDate);
                 
                 fondSumup = fondSumup + totalPercentage * (realStateSumup + stockSumup);
