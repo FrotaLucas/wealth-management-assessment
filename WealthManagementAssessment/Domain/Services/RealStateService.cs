8c541ffa398a4bdd3157ecfaca1cd50780e20ed9
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-18 20:56:25 +0200

    create constructor with IResository interface

--- WealthManagementAssessment/Domain/Services/RealStateService.cs
+++ WealthManagementAssessment/Domain/Services/RealStateService.cs
@@ -1,12 +1,22 @@
 ﻿using WealthManagementAssessment.Domain.Contracts.Interfaces;
+using WealthManagementAssessment.Domain.Contracts.Repository;
 using WealthManagementAssessment.Domain.Entities;
 
 namespace WealthManagementAssessment.Domain.Services
 {
     public class RealStateService : IRealStateService
     {
+        private readonly IPortfolioRepository _portfolioRepository;
+
+        public RealStateService(IPortfolioRepository portfolioRepository)
+        {
+            _portfolioRepository = portfolioRepository;
+        }
+
         public decimal RealStateEngine(List<Investment> investments)
         {
+
+            
 
             var realStateSumup = investments
                 .Where(investment => investment.InvestmentType == "RealEstate")
