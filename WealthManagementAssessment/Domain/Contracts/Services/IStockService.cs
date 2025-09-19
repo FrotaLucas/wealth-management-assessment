9139cef37280f607981a33b58ebae3d3efd5df37
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-19 12:08:29 +0200

    refactor parameter method

--- WealthManagementAssessment/Domain/Contracts/Services/IStockService.cs
+++ WealthManagementAssessment/Domain/Contracts/Services/IStockService.cs
@@ -5,6 +5,6 @@ namespace WealthManagementAssessment.Domain.Contracts.Interfaces
     public interface IStockService
     {
         //usar DECIMAL em tudo!!
-        decimal StockEngine(List<Investment> investments, DateTime valuationDate);
+        decimal StockEngine(string ownerid, DateTime valuationDate);
     }
 }
