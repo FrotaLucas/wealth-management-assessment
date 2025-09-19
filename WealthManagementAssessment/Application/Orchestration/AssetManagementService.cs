fa07bfd1e00aa1c784f83588d48ad96d171dd30f
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-19 14:03:52 +0200

    delete unused namespace

--- WealthManagementAssessment/Application/Orchestration/AssetManagementService.cs
+++ WealthManagementAssessment/Application/Orchestration/AssetManagementService.cs
@@ -1,6 +1,5 @@
 ﻿using WealthManagementAssessment.Application.Orchestration.Interfaces;
 using WealthManagementAssessment.Domain.Contracts.Interfaces;
-using WealthManagementAssessment.Domain.Entities;
 
 namespace WealthManagementAssessment.Application.Orchestration
 {
