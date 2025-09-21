3eec5f1c0d989d7f18d2252ac11bc2e81a5dc998
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-21 17:10:35 +0200

    fix interface

--- WealthManagementAssessment/Domain/Contracts/ProfileService.cs
+++ WealthManagementAssessment/Domain/Contracts/ProfileService.cs
@@ -1,7 +1,18 @@
-﻿namespace WealthManagementAssessment.Domain.Contracts
+﻿using WealthManagementAssessment.Domain.Contracts.Repository;
+
+namespace WealthManagementAssessment.Domain.Contracts
 {
     public class ProfileService
     {
 
+        private readonly IPortfolioRepository portfolioRepository;
+
+        public ProfileService(IPortfolioRepository portfolioRepository)
+        {
+            this.portfolioRepository = portfolioRepository;
+        }
+
+
+        
     }
 }
