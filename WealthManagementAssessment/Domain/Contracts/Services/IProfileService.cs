f753a047bae4fdb68a0a6d24e46e82c0aecf96f5
FrotaLucas <lucasdiasfrota@gmail.com>
2025-09-22 19:55:40 +0200

    assign method to interface

--- WealthManagementAssessment/Domain/Contracts/Services/IProfileService.cs
+++ WealthManagementAssessment/Domain/Contracts/Services/IProfileService.cs
@@ -1,7 +1,10 @@
-﻿namespace WealthManagementAssessment.Domain.Contracts.Services
+﻿using WealthManagementAssessment.Domain.Entities;
+
+namespace WealthManagementAssessment.Domain.Contracts.Services
 {
     public interface IProfileService
     {
         decimal ProfileEngine(string ownerId);
+
     }
 }
