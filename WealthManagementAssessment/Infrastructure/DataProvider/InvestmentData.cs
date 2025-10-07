f91e00868e8003352ad540cce6dec106cb483e05
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-07 20:12:45 +0200

    new class InvestmentData

---
+++ WealthManagementAssessment/Infrastructure/DataProvider/InvestmentData.cs
@@ -0,0 +1,20 @@
+﻿using WealthManagementAssessment.Domain.Enums;
+
+namespace WealthManagementAssessment.Infrastructure.DataProvider
+{
+    public class InvestmentData
+    {
+        public string InvestorId { get; set; }
+
+        public string InvestmentId { get; set; }
+
+        public InvestmentTypeEnum InvestmentType { get; set; }
+
+        public string ISIN { get; set; }
+
+        public string City { get; set; }
+
+        public string FondsInvestor { get; set; }
+
+    }
+}
