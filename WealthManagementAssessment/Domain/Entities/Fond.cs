b4852532c404beb3c0544e630f2b040abf576e92
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-07 16:05:17 +0200

    create class Fond

---
+++ WealthManagementAssessment/Domain/Entities/Fond.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WealthManagementAssessment.Domain.Entities
+{
+    public class Fond
+    {
+        public string FondsInvestor { get; set; } = default;
+    }
+}
