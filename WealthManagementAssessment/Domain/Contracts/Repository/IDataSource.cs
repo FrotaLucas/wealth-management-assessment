c041d35acdc1f0ca8244e60821a2ab99595cc749
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-07 20:16:45 +0200

    change Investment to investmentData

--- WealthManagementAssessment/Domain/Contracts/Repository/IDataSource.cs
+++ WealthManagementAssessment/Domain/Contracts/Repository/IDataSource.cs
@@ -1,10 +1,11 @@
 ﻿using WealthManagementAssessment.Domain.Entities;
+using WealthManagementAssessment.Infrastructure.DataProvider;
 
 namespace WealthManagementAssessment.Domain.Contracts.Repository
 {
     public interface IDataSource
     {
-        Dictionary<string, List<Investment>> ReadInvestments();
+        Dictionary<string, List<InvestmentData>> ReadInvestments();
 
         Dictionary<string, List<Transaction>> ReadTransactions();
 
