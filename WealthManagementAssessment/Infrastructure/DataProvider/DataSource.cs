0baa1d0e0cfcdf2cb1858c79bbc074c7d0b8d95e
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-07 20:21:22 +0200

    use investmentData

--- WealthManagementAssessment/Infrastructure/DataProvider/DataSource.cs
+++ WealthManagementAssessment/Infrastructure/DataProvider/DataSource.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Options;
 using WealthManagementAssessment.Application.Configuration;
 using WealthManagementAssessment.Domain.Contracts.Repository;
 using WealthManagementAssessment.Domain.Entities;
+using WealthManagementAssessment.Infrastructure.DataProvider;
 
 namespace WealthManagementAssessment.Infrastructure.Helper
 {
@@ -18,7 +19,7 @@ namespace WealthManagementAssessment.Infrastructure.Helper
             _appConfig = appConfig.Value;
         }
 
-        public Dictionary<string, List<Investment>> ReadInvestments()
+        public Dictionary<string, List<InvestmentData>> ReadInvestments()
         {
             var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
@@ -30,7 +31,7 @@ namespace WealthManagementAssessment.Infrastructure.Helper
             using (var reader = new StreamReader(_appConfig.CsvPath.Investments))
             using (var csv = new CsvReader(reader, config))
             {
-                var investments = csv.GetRecords<Investment>()
+                var investments = csv.GetRecords<InvestmentData>()
                      .GroupBy(investment => investment.InvestorId)
                      .ToDictionary(group => group.Key, group => group.ToList());
 
