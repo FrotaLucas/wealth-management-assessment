3f124fdc8b19238e51e982c3a8306d1976dcef28
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-08 18:20:00 +0200

    clean investment

--- WealthManagementAssessment/Domain/Entities/Investment.cs
+++ WealthManagementAssessment/Domain/Entities/Investment.cs
@@ -2,15 +2,11 @@
 
 namespace WealthManagementAssessment.Domain.Entities
 {
-    
     public abstract class Investment
     {
         public string InvestorId { get; set; } = default!;
 
         public string InvestmentId { get; set; } = default!;
-
-        //eliminar depois esse campo!!!
-        public InvestmentTypeEnum InvestmentType { get; set; }
 
         public List<Transaction> Transactions { get; set; } = new List<Transaction>();  
 
