740fa776d57a216c5eb82b4dd6069698ad33ee17
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-08 22:02:50 +0200

    finish dependencies of class

--- WealthManagementAssessment.Tests/AssetManagementServiceTests.cs
+++ WealthManagementAssessment.Tests/AssetManagementServiceTests.cs
@@ -1,5 +1,7 @@
 using Moq;
+using WealthManagementAssessment.Application.Orchestration;
 using WealthManagementAssessment.Domain.Contracts.Interfaces;
+using WealthManagementAssessment.Domain.Contracts.Services;
 
 namespace WealthManagementAssessment.Tests
 {
@@ -7,7 +9,31 @@ namespace WealthManagementAssessment.Tests
     {
         private readonly Mock<IStockService> _stockService;
 
-        private readonly Mock<IRealEstateService> _realStateService;
+        private readonly Mock<IRealEstateService> _realEstateService;
+
+        private readonly Mock<IFondService> _fondService;
+
+        private readonly Mock<IProfileService> _profileService;
+
+        private readonly AssetManagementService _assetManagementServiceTests;
+
+        public AssetManagementServiceTests()
+        {
+            _stockService = new Mock<IStockService>();  
+
+            _realEstateService = new Mock<IRealEstateService>();
+
+            _fondService = new Mock<IFondService>();    
+
+            _profileService = new Mock<IProfileService>();
+
+            _assetManagementServiceTests = new AssetManagementService(_stockService.Object,
+                _realEstateService.Object,
+                _fondService.Object,
+                _profileService.Object);
+
+
+        }
 
         [Fact]
         public void GetFondAsset_ShouldReturnExpectedFondBalance()
