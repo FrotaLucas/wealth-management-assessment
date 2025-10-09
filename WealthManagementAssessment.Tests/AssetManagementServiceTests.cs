b56dbfd8de9f24a79e7808f9987a93960c87e203
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-09 23:20:16 +0200

    new method

--- WealthManagementAssessment.Tests/AssetManagementServiceTests.cs
+++ WealthManagementAssessment.Tests/AssetManagementServiceTests.cs
@@ -98,6 +98,35 @@ namespace WealthManagementAssessment.Tests
             Assert.Equal(0, result.FondBalance);
         }
 
+        [Fact]
+        public void GetTotalAsset_ShouldReturnExpectedTotalBalance()
+        {
+            string ownerId = "Investor90";
+            DateTime valuationDate = new DateTime(2019, 12, 31);
+
+            decimal expetecedRealEstateAmount = 10000m;
+            decimal expetecedStockAmount = 20000m;
+            decimal expetecedFondAmount = 30000m;
+
+            _realEstateService.
+                Setup(x => x.RealStateEngine(ownerId, valuationDate))
+                .Returns(expetecedRealEstateAmount);
+
+            _stockService
+                .Setup(x => x.StockEngine(ownerId, valuationDate))
+                .Returns(expetecedStockAmount);
+
+            _fondService
+                .Setup(x => x.FondEngine(ownerId, valuationDate))   
+                .Returns(expetecedFondAmount);
+
+            var result = _assetManagementService.GetTotalAsset(ownerId, valuationDate);
+
+            Assert.Equal(expetecedStockAmount + expetecedFondAmount + expetecedRealEstateAmount, result.TotalBalance);
+            Assert.Equal(expetecedStockAmount, result.StockBalance);
+            Assert.Equal(expetecedFondAmount, result.FondBalance);
+            Assert.Equal(expetecedRealEstateAmount, result.RealStateBalance);
+        }
 
     }
 }
