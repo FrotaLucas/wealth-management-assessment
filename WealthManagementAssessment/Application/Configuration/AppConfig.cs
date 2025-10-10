4ad14e349c6045b5cdf8b2604da16e00bbfbc7e2
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-10 14:17:52 +0200

    extend AppConfig

--- WealthManagementAssessment/Application/Configuration/AppConfig.cs
+++ WealthManagementAssessment/Application/Configuration/AppConfig.cs
@@ -4,15 +4,25 @@
     {
 
         public CsvPathConfig CsvPath { get; set; } = new CsvPathConfig();
+       
+        public RiskProfileLimit RiskProfile {  get; set; } =  new RiskProfileLimit();
 
         public sealed class CsvPathConfig
         {
-            public string Investments { get; set; }
+            public string Investments { get; set; } = default!;
 
-            public string Transactions { get; set; }
+            public string Transactions { get; set; } = default!;    
 
-            public string Quotes { get; set; }
+            public string Quotes { get; set; } = default!;
 
         }
+
+        public sealed class RiskProfileLimit()
+        {
+            public decimal ConservativeUpperLimit { get; set; }
+
+            public decimal ModerateUpperLimit { get; set;  }
+        }
+
     }
 }
